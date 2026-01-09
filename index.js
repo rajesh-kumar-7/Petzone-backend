09f5197812dd4fb3672aa9c8e5fd5ac0f86c5dff
rajesh kumar <rajeshkumarwork7@gmail.com>
2026-01-09 11:33:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,11 +7,22 @@ import cookieparser from "cookie-parser"
 dotenv.config();
 import cors from 'cors'
 const app = express()
+app.set("trust proxy",1)
+
 app.use(cookieparser())
 app.use(express.json())
 app.use(cors({
-    origin: true,
-    credentials: true
+  origin: [
+    "http://localhost:5173",
+    "https://petzone.vercel.app",
+    "https://petzone-cv598ec46-rajesh-kumar-7s-projects.vercel.app",
+
+"https://petzone-git-main-rajesh-kumar-7s-projects.vercel.app",
+"https://petzone-pscgt4xpy-rajesh-kumar-7s-projects.vercel.app",
+  ],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
 }))
 
 const url = process.env.MONGO_URL;
